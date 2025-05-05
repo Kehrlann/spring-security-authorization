9ec212b6700868005f2d4ff71af0de1e566b4403
Daniel Garnier-Moiroux <git@garnier.wf>
2025-05-05 13:51:35 +0200

    demo: add tests for method security

--- src/test/java/wf/garnier/spring/security/authorization/AuthorizationApplicationTests.java
+++ src/test/java/wf/garnier/spring/security/authorization/AuthorizationApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
@@ -173,6 +174,13 @@ class AuthorizationApplicationTests {
 				var response = mvc.get().uri("/method/profile/test-user").exchange();
 
 				assertThat(response).hasStatus(HttpStatus.FORBIDDEN);
+			}
+
+			@Test
+			void methodProfileAnonymous() {
+				var response = mvc.get().uri("/method/profile/test-user").accept(MediaType.TEXT_HTML).exchange();
+
+				assertThat(response).hasStatus(HttpStatus.FOUND).redirectedUrl().endsWith("/login");
 			}
 
 		}
